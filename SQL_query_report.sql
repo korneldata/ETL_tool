86750e479a574e8806857d5882244f55507b3347
korneldata <126023589+korneldata@users.noreply.github.com>
2023-07-11 18:19:26 +0200

    Add files via upload

---
+++ SQL_query_report.sql
@@ -0,0 +1,15 @@
+SELECT 
+	 DATEPART(month, sales_date) as "month",
+	 DATEPART(year, sales_date) as "year",
+	 region,
+	 sum(operating_profit) as total_profit
+FROM report
+WHERE DATEPART(year, sales_date) = '2021'
+GROUP BY 
+GROUPING SETS (
+	(DATEPART(month, sales_date), DATEPART(year, sales_date), region),
+	(DATEPART(year, sales_date))
+)
+ORDER BY "month"
+
+
